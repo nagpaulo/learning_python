922f8d1d1a0360dc5bf14b45e534ee39a3bf557e
Paulo Roberto Mesquita da Silva <paulo.roberto@unifor.br>
2023-05-13 10:55:45 -0300

    Init projects

---
+++ Exercicio1.py
@@ -0,0 +1,33 @@
+# a = 1
+# print(a)
+
+# b = 1.0
+# print(b)
+
+# c = 1+0j
+# print(c)
+
+d = '1'
+# print(d)
+
+a, b, c, d = 1, 1.0, 1 + 0j,'1'
+print(a,b,c,d)
+
+
+# print(type(d))
+# dir(d)
+
+
+# class Caio: 
+#     def __dir__(self):
+#         self._do_import()
+#         global plot
+#         return dir(plot)
+
+# c = Caio()
+
+# dir(c)
+
+
+
+
